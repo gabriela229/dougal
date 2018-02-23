3597fc38fa582799d72afcae77401ff1dd6aa5ca
Gabriela Medina <gmedina229@gmail.com>
2018-02-23 06:11:15 -0500

    setup

---
+++ client/Components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Main() {
+
+    return (
+      <div className="row">
+        <h1 className="col-md-4 col-offset-4">Dougal's Page</h1>
+        <div className="row">
+          <p>Weclome to Dougal's page. If you're here, it means Dougal is lost.</p>
+          <p>Please contact Dougal's parents Gabriela Medina & Stephen Costello</p>
+          <p>Email: gmedina229@gmail.com or stephen.costello1991@gmail.com</p>
+          <p>Phone: 516-532-4559 or 347-640-1321</p>
+        </div>
+      </div>
+    );
+}
---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use('/dist', express.static(path.join(__dirname, '..', 'dist')));
+app.use('/vendor', express.static(path.join(__dirname, '..', 'node_modules')));
+app.use('/public', express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/*', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')));
+
+app.use(function (err, req, res, next) {
+  console.log(err, err.stack);
+  res.status(500).send(err.message);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
