61f00d4b2e27e0aa979e6e9eef0999387987e6d0
Gabriela Medina <gmedina229@gmail.com>
2018-02-26 08:29:52 -0500

    add grid, fix text

--- client/Components/Main.js
+++ client/Components/Main.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
-
 export default function Main() {
 
     return (
-      <div>
-        <h1 className="well">Dougal's Page</h1>
-        <div>
-          <p>Weclome to Dougal's page. If you're here, it means Dougal is lost.</p>
-          <p>Please contact Dougal's parents Gabriela Medina & Stephen Costello</p>
-          <p>Email: gmedina229@gmail.com or stephen.costello1991@gmail.com</p>
-          <p>Phone: 516-532-4559 or 347-640-1321</p>
+      <div className="container">
+        <div className="row">
+          <h1 className="mx-auto">Dougal Costello</h1>
+        </div>
+        <div className="row">
+          <div className="col-sm-6">
+            <div card>
+              <img className="card-img" src="/../../public/images/Dougal.jpg" alt="Card image" />
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="card">
+              <div className="card-body">
+                Welcome to Dougal's page. If you're here, it means Dougal is lost.
+                Please contact Dougal's parents Gabriela Medina & Stephen Costello
+                Email: gmedina229@gmail.com or stephen.costello1991@gmail.com
+                Phone: 516-532-4559 or 347-640-1321
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
